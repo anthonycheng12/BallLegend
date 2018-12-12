57cd42c5da8597521aa23e551247ffd8914db56e
chris-chao <christopherchao97@gmail.com>
2018-12-12 14:02:09 -0500

    fixed more things

--- BallLegend/BallElements.swift
+++ BallLegend/BallElements.swift
@@ -8,30 +8,52 @@ struct CollisionBitMask {
 }
 
 extension GameScene {
-    func createPlayer(legendChoice: String) -> SKSpriteNode {
+    func createScene() {
+        self.physicsBody = SKPhysicsBody(edgeLoopFrom: self.frame)
+        self.physicsBody?.categoryBitMask = CollisionBitMask.groundCategory
+        self.physicsBody?.collisionBitMask = CollisionBitMask.playerCategory // to get information on contact with player and obstacle
+        self.physicsBody?.contactTestBitMask = CollisionBitMask.playerCategory // to get information on contact with player and obstacle
+        self.physicsBody?.isDynamic = false
+        self.physicsBody?.affectedByGravity = false // prevents player from falling off the screen
         
-//         let player = SKSpriteNode(texture: SKTextureAtlas(named:"player").textureNamed(playerChoice))
-//
-//        let playerTexture = SKTexture(imageNamed: playerChoice);
-//        let player=SKSpriteNode(texture: playerTexture)
-//
-        // why do we need to declare set playerTexture?
+        self.physicsWorld.contactDelegate = self // helps detect contact and collisions
+        
+        
+        // for each i in range from 0 to less than 2
+        for i in 0 ..< 2
+        {
+            let background = SKSpriteNode(imageNamed: "bg")
+            background.anchorPoint = CGPoint.init(x: 0, y: 0)
+            background.position = CGPoint(x:CGFloat(i) * self.frame.width, y:0)
+            background.name = "background"
+            background.size = (self.view?.bounds.size)!
+            self.addChild(background)
+        } // this creates 2 backgrounds that are side by side and helps create the illusion of a neverending background
+        
+        self.player = createPlayer(legendChoice: playerChoice)
+        self.addChild(player)
+        
+        scoreLabel = yourScore()
+        self.addChild(scoreLabel)
+        
+        highscoreLabel = highScore()
+        self.addChild(highscoreLabel)
+        
+        appLogo()
+        
+        createStephButton()
+        createWadeButton()
+        
+        taptoplayLabel = playGame()
+        self.addChild(taptoplayLabel)
+    }
+    
+    func createPlayer(legendChoice: String) -> SKSpriteNode {
         let player = SKSpriteNode(imageNamed: playerChoice)
         
-        player.size = CGSize(width: 120, height: 140) // sets height and width of the character
-        player.position = CGPoint(x:self.frame.midX, y:self.frame.midY) // sets the player in the middle of the screen
-        
-         player.physicsBody = SKPhysicsBody(circleOfRadius: player.size.width / 4) // allows the player to behave like a real world object which is bothered by gravity and collisions
-    
-        // V2 for creating physicsbody
-//        player.physicsBody = SKPhysicsBody(texture: playerTexture,
-//                                           size: CGSize(width: player.size.width,
-//                                                        height: player.size.height))
-        
-        // V3 for creating physicsbody
-//        player.physicsBody = SKPhysicsBody(texture: player.texture!,
-//                                           size: player.texture!.size())
-        
+        player.size = CGSize(width: 120, height: 120) // sets height and width of the character
+        player.position = CGPoint(x:self.frame.midX, y: self.frame.midY) // sets the player in the middle of the screen
+        player.physicsBody = SKPhysicsBody(circleOfRadius: player.size.width / 4, center: CGPoint(x: 0, y: player.size.height / 4)) // allows the player to behave like a real world object which is bothered by gravity and collisions
         player.physicsBody?.linearDamping = 1.0
         player.physicsBody?.restitution = 0
         player.physicsBody?.categoryBitMask = CollisionBitMask.playerCategory // we notice collisions by comparing the category bit masks
@@ -43,8 +65,123 @@ extension GameScene {
         return player
     }
     
+    func selectPlayer(from touches: Set<UITouch>) {
+        for touch in touches {
+            let location = touch.location(in: self)
+            
+            if (stephButton.contains(location)){
+                playerChoice = "steph"
+            } else if (wadeButton.contains(location)) {
+                playerChoice = "wade"
+            }
+        }
+    }
+    
+    func resetPlayer() {
+        self.player.removeAllActions()
+        self.player.removeFromParent()
+        
+        self.player = createPlayer(legendChoice: playerChoice)
+        self.addChild(player)
+        
+        player.physicsBody?.affectedByGravity = true // have the player affected by gravity
+        player.physicsBody?.velocity = CGVector(dx: 0, dy: 0) // make the velocity 0 so it stays stable
+    }
+    
+    func start() {
+        resetPlayer()
+        
+        logoImage.run(SKAction.scale(to: 0.5, duration: 0.2), completion: {
+            self.logoImage.removeFromParent()
+        }) // removes the logo
+        
+        stephButton.run(SKAction.scale(to: 1, duration: 0.2), completion: {
+            self.stephButton.removeFromParent()
+        })
+        
+        wadeButton.run(SKAction.scale(to: 1, duration: 0.2), completion: {
+            self.wadeButton.removeFromParent()
+        })
+        
+        taptoplayLabel.removeFromParent() // removes the play button
+        
+        let spawn = SKAction.run({
+            () in
+            self.obstacle = self.createObstacle()
+            self.addChild(self.obstacle)
+        })
+        
+        let delay = SKAction.wait(forDuration: 1.5)
+        let SpawnDelay = SKAction.sequence([spawn, delay])
+        let spawnDelayForever = SKAction.repeatForever(SpawnDelay)
+        self.run(spawnDelayForever)
+        
+        let distance = CGFloat(self.frame.width + obstacle.frame.width)
+        let moveObstacles = SKAction.moveBy(x: -distance - 50, y: 0, duration: TimeInterval(0.008 * distance))
+        let removeObstacles = SKAction.removeFromParent()
+        moveAndRemove = SKAction.sequence([moveObstacles, removeObstacles])
+        
+        started = true
+    }
+    
+    func moveSprites() {
+        enumerateChildNodes(withName: "background", using: ({
+            (node, error) in
+            let bg = node as! SKSpriteNode
+            bg.position = CGPoint(x: bg.position.x - 2, y: bg.position.y)
+            if bg.position.x <= -bg.size.width {
+                bg.position = CGPoint(x:bg.position.x + bg.size.width * 2, y:bg.position.y)
+            }
+        }))
+    }
+    
+    func movePlayer() {
+        player.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
+        player.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 60)) // apply impulse to allow the player to go up and by how much
+    }
+    
+    func didRestart(from touches: Set<UITouch>) -> Bool {
+        print ("Called didRestart")
+        for touch in touches {
+            let location = touch.location(in: self)
+            
+            if restartButton.contains(location) {
+                return true
+            }
+        }
+        
+        return false
+    }
+    
+    func restart() {
+        print ("called restart")
+        resetPlayer()
+        updateHighScore()
+        
+        removeAllChildren()
+        removeAllActions()
+        
+        alive = true
+        started = false
+        score = 0
+        
+        createScene()
+    }
+    
     // this creates a restart for the game and shows the button on a screen
-    func restartGame() {
+    func gameOver() {
+        print ("gameover")
+        if !alive { return }
+        
+        updateHighScore()
+        
+        //        print (obstacle.children)
+        
+        alive = false
+        
+        obstacle.removeAllActions()
+        moveAndRemove = SKAction()
+        
         restartButton = SKSpriteNode(imageNamed: "restart")
         restartButton.size = CGSize(width:100, height:100)
         restartButton.position = CGPoint(x: self.frame.width / 2, y: self.frame.height / 2)
@@ -57,7 +194,7 @@ extension GameScene {
     //shows your score
     func yourScore() -> SKLabelNode {
         let scoreLabel = SKLabelNode()
-        scoreLabel.position = CGPoint(x: self.frame.width / 2, y: self.frame.height / 2 + self.frame.height / 2.6)
+        scoreLabel.position = CGPoint(x: self.frame.width / 2, y: self.frame.height - 125)
         scoreLabel.text = "\(score)"
         scoreLabel.zPosition = 5
         scoreLabel.fontSize = 50
@@ -74,15 +211,31 @@ extension GameScene {
         return scoreLabel
     }
     
+    func updateHighScore() {
+        if UserDefaults.standard.object(forKey: "highestScore") != nil {
+            let hscore = UserDefaults.standard.integer(forKey: "highestScore")
+            
+            if hscore < score {
+                UserDefaults.standard.set(scoreLabel.text, forKey: "highestScore")
+            }
+        } else {
+            UserDefaults.standard.set(0, forKey: "highestScore")
+        }
+    }
+    
     // takes note of a high score
     func highScore() -> SKLabelNode {
         let highscoreLabel = SKLabelNode()
-        highscoreLabel.position = CGPoint(x: self.frame.width - 80, y: self.frame.height - 22)
+        highscoreLabel.position = CGPoint(x: self.frame.width - 25, y: self.frame.height - 80)
+        highscoreLabel.numberOfLines = 2
+        highscoreLabel.horizontalAlignmentMode = .right
+        
         if let highestScore = UserDefaults.standard.object(forKey: "highestScore"){
-            highscoreLabel.text = "Highest Score: \(highestScore)"
+            highscoreLabel.text = "High Score\n\(highestScore) dunks!!!"
         } else {
-            highscoreLabel.text = "Highest Score: 0"
+            highscoreLabel.text = "High Score\n0 dunks!!!"
         }
+        
         highscoreLabel.zPosition = 5
         highscoreLabel.fontSize = 15
         highscoreLabel.fontName = "Courier-BoldOblique"
@@ -103,29 +256,19 @@ extension GameScene {
     func createStephButton() {
         stephButton = SKSpriteNode()
         stephButton = SKSpriteNode(imageNamed: "steph")
-        stephButton.size = CGSize(width: 250, height: 150)
-        stephButton.position = CGPoint(x:self.frame.midX-125, y:self.frame.midY-350)
-        stephButton.setScale(0.5)
+        stephButton.size = CGSize(width: 150, height: 150)
+        stephButton.position = CGPoint(x:self.frame.midX-110, y:self.frame.midY-325)
+        stephButton.setScale(1.0)
         self.addChild(stephButton)
         stephButton.run(SKAction.scale(to: 1.0, duration: 0.3))
     }
     
-//    func createLarryButton() {
-//        larryButton = SKSpriteNode()
-//        larryButton = SKSpriteNode(imageNamed: "larry")
-//        larryButton.size = CGSize(width: 300, height: 200)
-//        larryButton.position = CGPoint(x:self.frame.midX+35, y:self.frame.midY-300)
-//        larryButton.setScale(0.5)
-//        self.addChild(larryButton)
-//        larryButton.run(SKAction.scale(to: 1.0, duration: 0.3))
-//    }
-    
     func createWadeButton() {
         wadeButton = SKSpriteNode()
         wadeButton = SKSpriteNode(imageNamed: "wade")
-        wadeButton.size = CGSize(width: 250, height: 150)
+        wadeButton.size = CGSize(width: 150, height: 150)
         wadeButton.position = CGPoint(x:self.frame.midX+110, y:self.frame.midY-325)
-        wadeButton.setScale(0.5)
+        wadeButton.setScale(1.0)
         self.addChild(wadeButton)
         wadeButton.run(SKAction.scale(to: 1.0, duration: 0.3))
     }
@@ -142,61 +285,42 @@ extension GameScene {
         return taptoplayLabel
     }
     
-    func createObstacles() -> SKNode  {
-
-        let bball = SKSpriteNode(imageNamed: "hoop2")
-        bball.size = CGSize(width: 100, height: 100)
-        bball.position = CGPoint(x: self.frame.width + 45, y: self.frame.height / 2 - 50)
-        bball.physicsBody = SKPhysicsBody(rectangleOf: bball.size)
-        bball.physicsBody?.affectedByGravity = false
-        bball.physicsBody?.isDynamic = false
-        bball.physicsBody?.categoryBitMask = CollisionBitMask.basketballCategory // set to basketball so the program knows it is the ball
-        bball.physicsBody?.collisionBitMask = 0
-        bball.physicsBody?.contactTestBitMask = CollisionBitMask.playerCategory // contact with player will increase point by 1
-        bball.color = SKColor.white
-
-        obstaclePair = SKNode()
-        obstaclePair.name = "obstaclePair"
+    func createObstacle() -> SKNode  {
+        let hoop = SKSpriteNode(imageNamed: "hoop2")
+        hoop.size = CGSize(width: 100, height: 100)
+        hoop.position = CGPoint(x: self.frame.width + 45, y: self.frame.height / 2 - 50)
+        hoop.physicsBody = SKPhysicsBody(rectangleOf: hoop.size)
+        hoop.physicsBody?.affectedByGravity = false
+        hoop.physicsBody?.isDynamic = false
+        hoop.physicsBody?.categoryBitMask = CollisionBitMask.basketballCategory // set to basketball so the program knows it is the ball
+        hoop.physicsBody?.collisionBitMask = 0
+        hoop.physicsBody?.contactTestBitMask = CollisionBitMask.playerCategory // contact with player will increase point by 1
+        hoop.color = SKColor.white
         
-//        let topObs = SKSpriteNode(imageNamed: "shaq")
-        let botObs = SKSpriteNode(imageNamed: "signpost")
-        botObs.size = CGSize(width: 40, height: 600);
+        obstacle = SKNode()
+        obstacle.name = "obstacle"
         
-//        topObs.position = CGPoint(x: self.frame.width + 25, y: self.frame.height / 2 + 400)
-        botObs.position = CGPoint(x: self.frame.width + 50, y: self.frame.height/2 - 400) // the add and minus contributes to the space between the top obstacle and bottom obstacle
+        let signPost = SKSpriteNode(imageNamed: "signpost")
+        signPost.size = CGSize(width: 40, height: 600);
+        signPost.position = CGPoint(x: self.frame.width + 50, y: self.frame.height/2 - 400) // the add and minus contributes to the space between the top obstacle and the ceiling
+        signPost.setScale(1.0)
         
-//        topObs.setScale(0.5)
-        botObs.setScale(1.0)
+        signPost.physicsBody = SKPhysicsBody(rectangleOf: signPost.size)
+        signPost.physicsBody?.categoryBitMask = CollisionBitMask.obstacleCategory
+        signPost.physicsBody?.collisionBitMask = CollisionBitMask.playerCategory
+        signPost.physicsBody?.contactTestBitMask = CollisionBitMask.playerCategory
+        signPost.physicsBody?.isDynamic = false
+        signPost.physicsBody?.affectedByGravity = false
         
-//        topObs.physicsBody = SKPhysicsBody(rectangleOf: topObs.size)
-//        topObs.physicsBody?.categoryBitMask = CollisionBitMask.obstacleCategory
-//        topObs.physicsBody?.collisionBitMask = CollisionBitMask.playerCategory
-//        topObs.physicsBody?.contactTestBitMask = CollisionBitMask.playerCategory
-//        topObs.physicsBody?.isDynamic = false
-//        topObs.physicsBody?.affectedByGravity = false
+        obstacle.addChild(signPost)
+        obstacle.zPosition = 1
         
-        botObs.physicsBody = SKPhysicsBody(rectangleOf: botObs.size)
-        botObs.physicsBody?.categoryBitMask = CollisionBitMask.obstacleCategory
-        botObs.physicsBody?.collisionBitMask = CollisionBitMask.playerCategory
-        botObs.physicsBody?.contactTestBitMask = CollisionBitMask.playerCategory
-        botObs.physicsBody?.isDynamic = false
-        botObs.physicsBody?.affectedByGravity = false
+        let randomPosition = CGFloat.random(min: -175, max: 200) // higher the number the longer the obstacle can potentially be
+        obstacle.position.y = obstacle.position.y + randomPosition
+        obstacle.addChild(hoop)
         
-//        topObs.zRotation = CGFloat(Double.pi)
+        obstacle.run(moveAndRemove)
         
-//        obstaclePair.addChild(topObs)
-        obstaclePair.addChild(botObs)
-        
-        obstaclePair.zPosition = 1
-
-        let randomPosition = CGFloat.random(min: -175, max: 200) // higher the number the longer the obstacles can potentially be
-        print (randomPosition)
-        obstaclePair.position.y = obstaclePair.position.y + randomPosition
-        obstaclePair.addChild(bball)
-        
-        obstaclePair.run(moveAndRemove)
-        
-        return obstaclePair
+        return obstacle
     }
-    
 }
--- BallLegend/GameScene.swift
+++ BallLegend/GameScene.swift
@@ -2,224 +2,71 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene , SKPhysicsContactDelegate {
-
-    var didGameStarted = Bool(false)
-    var didDied = Bool(false)
+    
+    // MARK: - Properties
+    
     let buzzSound = SKAction.playSoundFileNamed("buzzer.mp3", waitForCompletion: false)
-    var score = Int(0)
+    
+    // MARK: - State
+    
+    var started: Bool = false
+    var alive: Bool = true
+    var score: Int = 0
+    var playerChoice = "steph"
+    
+    // MARK: - Sprites
+    
     var scoreLabel = SKLabelNode()
     var highscoreLabel = SKLabelNode()
     var taptoplayLabel = SKLabelNode()
     var restartButton = SKSpriteNode()
-    var pauseButton = SKSpriteNode()
     var logoImage = SKSpriteNode()
-    var obstaclePair = SKNode()
+    var obstacle = SKNode()
     var moveAndRemove = SKAction()
     let playerAtlas = SKTextureAtlas(named:"player")
     var playerSprites = Array<SKTexture>()
     var player = SKSpriteNode()
-    var repeatPlayerAction = SKAction()
-    
     var stephButton = SKSpriteNode()
-//    var larryButton = SKSpriteNode()
     var wadeButton = SKSpriteNode()
-    var playerChoice = "steph"
-
+    
     override func didMove(to view: SKView) {
         createScene()
         view.showsPhysics = true
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if didGameStarted == false{
-            
-            for touch in touches{
-                let location = touch.location(in: self)
-                if (stephButton.contains(location)){
-                    playerChoice = "steph"
-                    print ("chose steph")
-                }
-//                else if (larryButton.contains(location)){
-//                    playerChoice = "larry"
-//                    print ("chose larry")
-//                }
-                else if (wadeButton.contains(location)){
-                    playerChoice = "wade"
-                    print ("chose wade")
-                }
-            }
-            
-            
-            didGameStarted =  true // check to see if the game started
-            player.physicsBody?.affectedByGravity = true // have the player affected by gravity
-            logoImage.run(SKAction.scale(to: 0.5, duration: 0.2), completion: {
-                self.logoImage.removeFromParent()
-            }) // removes the logo
-            
-            
-            
-            stephButton.run(SKAction.scale(to: 1, duration: 0.2), completion: {
-                self.stephButton.removeFromParent()
-            })
-//            larryButton.run(SKAction.scale(to: 1, duration: 0.2), completion: {
-//                self.larryButton.removeFromParent()
-//            })
-            wadeButton.run(SKAction.scale(to: 1, duration: 0.2), completion: {
-                self.wadeButton.removeFromParent()
-            })
-
-            
-            
-            taptoplayLabel.removeFromParent() // removes the play button
-            // self.player.run(repeatPlayerAction) potentially remove this (just animates the bird)
-            
-            let spawn = SKAction.run({
-                () in
-                self.obstaclePair = self.createObstacles()
-                self.addChild(self.obstaclePair)
-            })
-            
-            let delay = SKAction.wait(forDuration: 1.5)
-            let SpawnDelay = SKAction.sequence([spawn, delay])
-            let spawnDelayForever = SKAction.repeatForever(SpawnDelay)
-            self.run(spawnDelayForever)
-
-            let distance = CGFloat(self.frame.width + obstaclePair.frame.width)
-            let moveObstacles = SKAction.moveBy(x: -distance - 50, y: 0, duration: TimeInterval(0.008 * distance))
-            let removeObstacles = SKAction.removeFromParent()
-            moveAndRemove = SKAction.sequence([moveObstacles, removeObstacles])
-            
-            player.physicsBody?.velocity = CGVector(dx: 0, dy: 0) // make the velocity 0 so it stays stable
-            player.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 60)) // apply impulse to allow the player to go up and by how much
-        } else {
-            if didDied == false {
-                player.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
-                player.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 60))
-            }
-            // as long as you are not dead keep the velocity at 0 and then have the player go up and by how much
-        }
-        
-        for touch in touches{
-            let location = touch.location(in: self)
-
-            if didDied == true{
-                if restartButton.contains(location){
-                    if UserDefaults.standard.object(forKey: "highestScore") != nil {
-                        let hscore = UserDefaults.standard.integer(forKey: "highestScore")
-                        if hscore < Int(scoreLabel.text!)!{
-                            UserDefaults.standard.set(scoreLabel.text, forKey: "highestScore")
-                        }
-                    } else {
-                        UserDefaults.standard.set(0, forKey: "highestScore")
-                    }
-                    restartScene()
-                }
-            }
+        if !started {
+            selectPlayer(from: touches)
+            start()
+        } else if alive {
+            movePlayer()
+        } else if didRestart(from: touches) {
+            restart()
         }
     }
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered to move the background slowly 2 pixels to the left each time the function is called
-        if didGameStarted == true{
-            if didDied == false{
-                enumerateChildNodes(withName: "background", using: ({
-                    (node, error) in
-                    let bg = node as! SKSpriteNode
-                    bg.position = CGPoint(x: bg.position.x - 2, y: bg.position.y)
-                    if bg.position.x <= -bg.size.width {
-                        bg.position = CGPoint(x:bg.position.x + bg.size.width * 2, y:bg.position.y)
-                    }
-                }))
-            }
+        if (started && alive) {
+            moveSprites()
         }
     }
     
     func didBegin(_ contact: SKPhysicsContact) {
-        let firstBody = contact.bodyA
-        let secondBody = contact.bodyB
+        if (![contact.bodyA.categoryBitMask, contact.bodyB.categoryBitMask].contains(CollisionBitMask.playerCategory)) { return }
         
-        // checking if player contacts ground or obstacle
-        if firstBody.categoryBitMask == CollisionBitMask.groundCategory && secondBody.categoryBitMask == CollisionBitMask.obstacleCategory || firstBody.categoryBitMask == CollisionBitMask.obstacleCategory && secondBody.categoryBitMask == CollisionBitMask.playerCategory || firstBody.categoryBitMask == CollisionBitMask.playerCategory && secondBody.categoryBitMask == CollisionBitMask.groundCategory || firstBody.categoryBitMask == CollisionBitMask.groundCategory && secondBody.categoryBitMask == CollisionBitMask.playerCategory{
-            enumerateChildNodes(withName: "obstaclePair", using: ({
-                (node, error) in
-                node.speed = 0
-                self.removeAllActions()
-            }))
-            if didDied == false{
-                didDied = true
-                restartGame()
-                self.player.removeAllActions()
-            }
-        }
-        // checking if player contacts the basketball
-        else if firstBody.categoryBitMask == CollisionBitMask.playerCategory && secondBody.categoryBitMask == CollisionBitMask.basketballCategory {
+        // if bodyA is the player then collisionBody = bodyB, otherwise collisionBody = bodyA
+        let collisionBody = contact.bodyA.categoryBitMask == CollisionBitMask.playerCategory ? contact.bodyB : contact.bodyA
+        
+        // if the player contacts a hoop
+        if collisionBody.categoryBitMask == CollisionBitMask.basketballCategory {
             run(buzzSound)
             score += 1
             scoreLabel.text = "\(score)"
-            secondBody.node?.removeFromParent()
+            collisionBody.node?.removeFromParent()
+        } else { // if the player contacts anything else
+            gameOver()
+            //            restart()
         }
-        // checking if basketball contacts the player
-        else if firstBody.categoryBitMask == CollisionBitMask.basketballCategory && secondBody.categoryBitMask == CollisionBitMask.playerCategory {
-            run(buzzSound)
-            score += 1
-            scoreLabel.text = "\(score)"
-            firstBody.node?.removeFromParent()
-        }
-    }
-    
-    func restartScene(){
-        self.removeAllChildren()
-        self.removeAllActions()
-        didDied = false
-        didGameStarted = false
-        score = 0
-        createScene()
-    }
-    
-    func createScene(){
-        self.physicsBody = SKPhysicsBody(edgeLoopFrom: self.frame)
-        self.physicsBody?.categoryBitMask = CollisionBitMask.groundCategory
-        self.physicsBody?.collisionBitMask = CollisionBitMask.playerCategory // to get information on contact with player and obstacle
-        self.physicsBody?.contactTestBitMask = CollisionBitMask.playerCategory // to get information on contact with player and obstacle
-        self.physicsBody?.isDynamic = false
-        self.physicsBody?.affectedByGravity = false // prevents player from falling off the screen
-        
-        self.physicsWorld.contactDelegate = self // helps detect contact and collisions
-        // self.backgroundColor = SKColor(red: 80.0/255.0, green: 192.0/255.0, blue: 203.0/255.0, alpha: 1.0)
-        
-        // for each i in range from 0 to less than 2
-        for i in 0..<2
-        {
-            let background = SKSpriteNode(imageNamed: "bg")
-            background.anchorPoint = CGPoint.init(x: 0, y: 0)
-            background.position = CGPoint(x:CGFloat(i) * self.frame.width, y:0)
-            background.name = "background"
-            background.size = (self.view?.bounds.size)!
-            self.addChild(background)
-        } // this creates 2 backgrounds that are side by side and helps create the illusion of a neverending background
-        
-        
-        playerSprites.append(playerAtlas.textureNamed(playerChoice)) // shows the player
-        
-        self.player = createPlayer(legendChoice: playerChoice)
-        self.addChild(player)
-        
-//        let animatePlayer = SKAction.animate(with: self.playerSprites, timePerFrame: 0.1)
-//        self.repeatPlayerAction = SKAction.repeatForever(animatePlayer)
-        
-        scoreLabel = yourScore()
-        self.addChild(scoreLabel)
-        
-        highscoreLabel = highScore()
-        self.addChild(highscoreLabel)
-        
-        appLogo()
-        
-        createStephButton()
-//        createLarryButton()
-        createWadeButton()
-
-        taptoplayLabel = playGame()
-        self.addChild(taptoplayLabel)
     }
 }
